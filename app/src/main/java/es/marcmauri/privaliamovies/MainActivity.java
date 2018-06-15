bada201323acd7a02196ee134b38d75f56c664d1
Marc Mauri Ruiz <marcmauri92@gmail.com>
2018-06-15 04:21:03 -0700

    MovieListFragment bind to MainActivity

--- app/src/main/java/es/marcmauri/privaliamovies/MainActivity.java
+++ app/src/main/java/es/marcmauri/privaliamovies/MainActivity.java
@@ -1,13 +1,40 @@
 package es.marcmauri.privaliamovies;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView toolbar_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindUI();
+        setFragmentByDefault();
+    }
+
+    private void setFragmentByDefault() {
+        changeFragmentToPopularMovies();
+    }
+
+    private void changeFragmentToPopularMovies() {
+        changeFragment(new MovieListFragment());
+    }
+
+    private void changeFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+    }
+
+    private void bindUI() {
+        this.toolbar_title = findViewById(R.id.toolbar_title);
     }
 }
